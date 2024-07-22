c38a6a55d6eed543e4f9c3fa2a6d1239af44c593
LTS <94734905+lin911220@users.noreply.github.com>
2024-07-22 16:06:21 +0800

    Add files via upload
    window system (2)

---
+++ 0722/close.py
@@ -0,0 +1,16 @@
+import os
+import platform
+
+def shutdown():
+    system_platform = platform.system()
+    if system_platform == 'Windows':
+        os.system('shutdown /s /t 1')  # Windows close
+    elif system_platform == 'Linux':
+        os.system('sudo shutdown now')  # Linux close
+    elif system_platform == 'Darwin':
+        os.system('sudo shutdown -h now')  # macOS close
+    else:
+        print("Unsupported OS for shutdown")
+
+if __name__ == '__main__':
+     shutdown()
